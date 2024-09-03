d5da9d2311d8f030ca0d745f0f8a47f318581600
Martin Nohr <martinnohr76@gmail.com>
2024-09-03 16:47:38 -0600

    Added experimental web server page.

--- HollowClockEsp32.ino
+++ HollowClockEsp32.ino
@@ -32,6 +32,10 @@ struct {
     long utcOffsetInSeconds = -7 * 3600;
 } settings;
 
+// Set web server port number to 80
+WiFiServer server(80);
+bool bWifiConnected = false;
+
 // ports used to control the stepper motor
 // if your motor rotates in the opposite direction, 
 // change the order as {2, 3, 4, 5};
@@ -76,6 +80,7 @@ void rotate(int step)
 TaskHandle_t TaskMenuHandle;
 TaskHandle_t TaskClockMinuteHandle;
 TaskHandle_t TaskWifiHandle;
+TaskHandle_t TaskServerHandle;
 volatile bool bGotTime = false;
 struct tm gtime;
 
@@ -241,6 +246,9 @@ void TaskWiFi(void* params)
 			settings.cWifiPWD[0] = '\0';
 			Serial.println("Network timed out, clearing network setting");
 		}
+		else {
+			bWifiConnected = true;
+		}
 		Serial.println("");
 		Serial.println(String("ip:") + WiFi.localIP().toString());
 		configTime(settings.utcOffsetInSeconds, /* daylight saving */0, "pool.ntp.org");
@@ -261,6 +269,108 @@ void TaskWiFi(void* params)
 		Serial.println("Time check : " + String(gtime.tm_hour) + ":" + gtime.tm_min);
 		vTaskDelayUntil(&xLastWakeTime, xFrequency);
     }
+}
+
+/*
+* handle the web server for adjusting settings
+*/
+void TaskServer(void* params)
+{
+	String header;
+	unsigned long currentTime = millis();
+	// Previous time
+	unsigned long previousTime = 0;
+	// Define timeout time in milliseconds (example: 2000ms = 2s)
+	const long timeoutTime = 2000;
+	// wait for WiFi to be ready
+	while (!bWifiConnected)
+		vTaskDelay(pdMS_TO_TICKS(1000));
+	server.begin();
+	bool ledState = false;
+	for (;;) {
+		WiFiClient client = server.available();   // Listen for incoming clients
+
+		if (client) {                             // If a new client connects,
+			currentTime = millis();
+			previousTime = currentTime;
+			Serial.println("New Client.");          // print a message out in the serial port
+			String currentLine = "";                // make a String to hold incoming data from the client
+			while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+				currentTime = millis();
+				if (client.available()) {             // if there's bytes to read from the client,
+					char c = client.read();             // read a byte, then
+					Serial.write(c);                    // print it out the serial monitor
+					header += c;
+					if (c == '\n') {                    // if the byte is a newline character
+						// if the current line is blank, you got two newline characters in a row.
+						// that's the end of the client HTTP request, so send a response:
+						if (currentLine.length() == 0) {
+							// HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+							// and a content-type so the client knows what's coming, then a blank line:
+							client.println("HTTP/1.1 200 OK");
+							client.println("Content-type:text/html");
+							client.println("Connection: close");
+							client.println();
+							//Serial.println(header);
+							// turns the GPIOs on and off
+							if (header.indexOf("GET /26/on") >= 0) {
+								Serial.println("GPIO 26 on");
+								ledState = false;
+							}
+							else if (header.indexOf("GET /26/off") >= 0) {
+								Serial.println("GPIO 26 off");
+								ledState = true;
+							}
+
+							// Display the HTML web page
+							client.println("<!DOCTYPE html><html>");
+							client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+							client.println("<link rel=\"icon\" href=\"data:,\">");
+							// CSS to style the on/off buttons 
+							// Feel free to change the background-color and font-size attributes to fit your preferences
+							client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+							client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+							client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+							client.println(".button2 {background-color: #555555;}</style></head>");
+
+							// Web Page Heading
+							client.println("<body><h1>Hollow Clock</h1>");
+
+							// Display current state, and ON/OFF buttons for GPIO 26  
+							client.println(String("<p>GPIO 26 - State ") + (ledState ? "on" : "off") + "</p>");
+							// If the output26State is off, it displays the ON button       
+							if (ledState) {
+								client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
+							}
+							else {
+								client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
+							}
+
+							client.println("</body></html>");
+
+							// The HTTP response ends with another blank line
+							client.println();
+							// Break out of the while loop
+							break;
+						}
+						else { // if you got a newline, then clear currentLine
+							currentLine = "";
+						}
+					}
+					else if (c != '\r') {  // if you got anything else but a carriage return character,
+						currentLine += c;      // add it to the end of the currentLine
+					}
+				}
+			}
+			// Clear the header variable
+			header = "";
+			// Close the connection
+			client.stop();
+			Serial.println("Client disconnected.");
+			Serial.println("");
+		}
+		vTaskDelay(pdMS_TO_TICKS(100));
+	}
 }
 
 void setup()
@@ -297,13 +407,14 @@ void setup()
     xTaskCreate(TaskMinutes, "MINUTES", 1000, NULL, 3, &TaskClockMinuteHandle);
     xTaskCreate(TaskMenu, "MENU", 2000, NULL, 4, &TaskMenuHandle);
     xTaskCreate(TaskWiFi, "WIFI", 4000, NULL, 2, &TaskWifiHandle);
+	xTaskCreate(TaskServer, "SERVER", 10000, NULL, 5, &TaskServerHandle);
 }
 
 void loop()
 {
     if (settings.bTestMode) {
         // just run the motor
-        rotate(1000);
+		rotate(60 * STEPS_PER_MIN);
     }
     vTaskDelay(pdMS_TO_TICKS(1000));
 }
